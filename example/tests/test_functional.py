4692da44ab4699b6ae9c68c3dda8d77f0d27d5ea
gabe gaster <gabe@gabegaster.com>
2015-04-19 11:09:06 -0500

    add selenium test

---
+++ example/tests/test_functional.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
+from django.test import LiveServerTestCase
+from django.template import Context, Template
+from django.template.base import VariableNode
+
+from offlinecdn.conf import settings
+
+class StaticTest(LiveServerTestCase):
+    browser = None
+    host = "http://localhost:8081"
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def test_server_running(self):
+        r = requests.get(self.host)
+        self.assertTrue(r.ok)
+
+    def test_functional(self):
+        self.browser.get(self.host)
+        dom = self.browser.page_source
+        soup = BeautifulSoup(dom)
+        text = soup.find("p").text
+        self.assertNotIn("no", text)
+
+    def tearDown(self):
+        self.browser.close()
