22afac36a1276e42adcd86b6e7d9609cdc8592a7
gabe gaster <gabe@gabegaster.com>
2015-04-20 09:42:57 -0500

    trying to run headless

--- example/tests/test_functional.py
+++ example/tests/test_functional.py
@@ -29,3 +29,9 @@ class StaticTest(StaticLiveServerTestCase):
 
     def tearDown(self):
         self.browser.close()
+
+# username = os.environ["SAUCE_USERNAME"]
+# access_key = os.environ["SAUCE_ACCESS_KEY"]
+# capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
+# hub_url = "%s:%s@localhost:4445" % (username, access_key)
+# driver = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % hub_url)
